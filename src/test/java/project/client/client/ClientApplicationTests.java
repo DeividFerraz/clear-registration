229c40110fb33c808e687442f612f20e12c4a9a0
DeividFerraz <deividferraz20@gmail.com>
2023-08-16 00:33:20 -0300

    first commit

---
+++ src/test/java/project/client/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package project.client.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
