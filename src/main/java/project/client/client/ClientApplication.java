704a20310becd57b47f2696cdf53242fa0870338
deividferraz20@gmail.com <deividferraz20@gmail.com>
2023-08-18 01:52:07 -0300

    aaa2222222

--- src/main/java/project/client/client/ClientApplication.java
+++ src/main/java/project/client/client/ClientApplication.java
@@ -1,11 +1,16 @@
 package project.client.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientApplication {
 
+	
+	@Value("${Postgres.DATABASE_URL}")
+    private String databaseUrl;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
