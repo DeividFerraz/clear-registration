049be80f5b8c478bf563b5f5800ae736a419914a
deividferraz20@gmail.com <deividferraz20@gmail.com>
2023-08-18 02:08:39 -0300

    profile add

--- src/main/java/project/client/client/ClientApplication.java
+++ src/main/java/project/client/client/ClientApplication.java
@@ -2,8 +2,10 @@ package project.client.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("prod")
 public class ClientApplication {
 
 	public static void main(String[] args) {
